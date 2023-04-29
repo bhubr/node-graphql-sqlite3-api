b56dd06d5d475f41bc037d30e990f3aa8a511950
Benoît Hubert <benoithubert@gmail.com>
2023-04-29 17:01:14 +0200

    add resolver

--- src/index.js
+++ src/index.js
@@ -32,3 +32,12 @@ const books = [
   },
 ];
 
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+
